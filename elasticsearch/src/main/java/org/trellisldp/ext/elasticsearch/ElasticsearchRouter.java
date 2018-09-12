e4b2466cd19a8e0632628794dba5dd53749a13b0
Aaron Coburn <acoburn@apache.org>
2018-09-12 14:16:00 -0400

    Add elasticsearch connector
    Resolves #1

---
+++ elasticsearch/src/main/java/org/trellisldp/ext/elasticsearch/ElasticsearchRouter.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.ext.elasticsearch;
+
+import static java.net.URLEncoder.encode;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.camel.Exchange.CONTENT_TYPE;
+import static org.apache.camel.Exchange.HTTP_METHOD;
+import static org.apache.camel.Exchange.HTTP_QUERY;
+import static org.apache.camel.Exchange.HTTP_URI;
+import static org.apache.camel.LoggingLevel.INFO;
+import static org.apache.camel.builder.PredicateBuilder.and;
+import static org.apache.camel.component.http4.HttpMethods.DELETE;
+import static org.apache.camel.component.http4.HttpMethods.GET;
+import static org.apache.camel.component.http4.HttpMethods.PUT;
+import static org.apache.camel.model.dataformat.JsonLibrary.Jackson;
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.trellisldp.camel.ActivityStreamProcessor.ACTIVITY_STREAM_OBJECT_ID;
+import static org.trellisldp.camel.ActivityStreamProcessor.ACTIVITY_STREAM_TYPE;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.trellisldp.camel.ActivityStreamProcessor;
+
+/**
+ * A routing engine for indexing LDP content in Elasticsearch.
+ */
+public class ElasticsearchRouter extends RouteBuilder {
+
+    private static final Logger LOGGER = getLogger(ElasticsearchRouter.class);
+
+    @Override
+    public void configure() throws Exception {
+        from("{{input.stream}}").routeId("TrellisElasticsearchRouter")
+            .unmarshal().json(Jackson)
+            .process(new ActivityStreamProcessor())
+            .filter(and(
+                        header(ACTIVITY_STREAM_OBJECT_ID).isNotNull(),
+                        simple("'{{ldpath.service.url}}' regex '^https?://.+'"),
+                        simple("'{{elasticsearch.url}}' regex '^https?://.+'")))
+                .process(e -> e.getIn().setHeader("ElasticSearchId",
+                            encode(e.getIn().getHeader(ACTIVITY_STREAM_OBJECT_ID, String.class), UTF_8)))
+                .choice()
+                    .when(header(ACTIVITY_STREAM_TYPE).contains("Delete"))
+                        .to("direct:delete.elasticsearch")
+                    .otherwise()
+                        .to("direct:fetch.resource");
+
+        from("direct:delete.elasticsearch").routeId("TrellisElasticsearchDeleter")
+            .log(INFO, LOGGER, "Deleting ${headers.ActivityStreamObjectId} from elasticsearch")
+            .removeHeaders("CamelHttp*")
+            .setHeader(HTTP_URI).simple("{{elasticsearch.url}}${headers.ElasticSearchId}")
+            .setHeader(HTTP_METHOD).constant(DELETE)
+            .to("http4://localhost?useSystemProperties=true");
+
+        from("direct:fetch.resource").routeId("TrellisLdpathFormatter")
+            .log(INFO, LOGGER, "Fetching resource via LDPath: ${headers.ActivityStreamObjectId}")
+            .removeHeaders("CamelHttp*")
+            .setHeader(HTTP_URI).simple("{{ldpath.service.url}}")
+            .setHeader(HTTP_METHOD).constant(GET)
+            .setHeader(HTTP_QUERY).simple("url=${headers.ActivityStreamObjectId}&program={{ldpath.program.url}}")
+            .to("http4://localhost?useSystemProperties=true")
+            .to("direct:update.elasticsearch");
+
+        from("direct:update.elasticsearch").routeId("TrellisElasticsearchUpdater")
+            .removeHeaders("CamelHttp*")
+            .setHeader(HTTP_URI).simple("{{elasticsearch.url}}${headers.ElasticSearchId}")
+            .setHeader(HTTP_METHOD).constant(PUT)
+            .setHeader(CONTENT_TYPE).constant("application/json")
+            .to("http4://localhost?useSystemProperties=true");
+    }
+}
+
---
+++ elasticsearch/src/test/java/org/trellisldp/ext/elasticsearch/ElasticsearchRouterTest.java
@@ -0,0 +1,169 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.ext.elasticsearch;
+
+import static org.apache.camel.Exchange.CONTENT_TYPE;
+import static org.apache.camel.Exchange.HTTP_METHOD;
+import static org.apache.camel.Exchange.HTTP_QUERY;
+import static org.apache.camel.Exchange.HTTP_URI;
+import static org.apache.camel.component.http4.HttpMethods.DELETE;
+import static org.apache.camel.component.http4.HttpMethods.GET;
+import static org.apache.camel.component.http4.HttpMethods.PUT;
+import static org.apache.camel.util.ObjectHelper.loadResourceAsStream;
+
+import java.util.Properties;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.junit.Test;
+
+public class ElasticsearchRouterTest extends CamelBlueprintTestSupport {
+
+    @EndpointInject(uri = "mock:results")
+    protected MockEndpoint resultEndpoint;
+
+    @Produce(uri = "direct:start")
+    protected ProducerTemplate template;
+
+    @Override
+    public boolean isUseAdviceWith() {
+        return true;
+    }
+
+    @Override
+    public boolean isUseRouteBuilder() {
+        return false;
+    }
+
+    @Override
+    protected String getBlueprintDescriptor() {
+        return "/OSGI-INF/blueprint/blueprint.xml";
+    }
+
+    @Override
+    protected Properties useOverridePropertiesWithPropertiesComponent() {
+        final Properties props = new Properties();
+        props.setProperty("input.stream", "seda:input");
+        props.setProperty("elasticsearch.url", "https://elasticsearch.example.com/ldp/_doc/");
+        props.setProperty("ldpath.service.url", "https://ldpath.example.com");
+        props.setProperty("ldpath.program.url", "https://ldpath.example.com/program.ldpath");
+        return props;
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        context.getRouteDefinition("TrellisLdpathFormatter").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                mockEndpointsAndSkip("http*");
+            }
+        });
+        context.getRouteDefinition("TrellisElasticsearchUpdater").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveAddLast().to("mock:results");
+                mockEndpointsAndSkip("http*");
+            }
+        });
+        context.start();
+
+        getMockEndpoint("mock:http4:localhost").expectedMessageCount(2);
+        getMockEndpoint("mock:http4:localhost").expectedHeaderValuesReceivedInAnyOrder(
+                HTTP_METHOD, GET, PUT);
+
+        resultEndpoint.expectedMessageCount(1);
+        resultEndpoint.expectedHeaderReceived(CONTENT_TYPE, "application/json");
+        resultEndpoint.expectedHeaderReceived(HTTP_URI,
+                "https://elasticsearch.example.com/ldp/_doc/https%3A%2F%2Fldp.example.com%2Fresource");
+        resultEndpoint.expectedHeaderReceived(HTTP_METHOD, PUT);
+
+        template.sendBody("seda:input", loadResourceAsStream("create.jsonld"));
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void testFetch() throws Exception {
+        context.getRouteDefinition("TrellisLdpathFormatter").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                mockEndpointsAndSkip("http*");
+                mockEndpointsAndSkip("direct:update.elasticsearch");
+                weaveAddLast().to("mock:results");
+            }
+        });
+        context.start();
+
+        resultEndpoint.expectedMessageCount(1);
+        resultEndpoint.expectedHeaderReceived(HTTP_URI, "https://ldpath.example.com");
+        resultEndpoint.expectedHeaderReceived(HTTP_QUERY,
+                "url=https://ldp.example.com/resource&program=https://ldpath.example.com/program.ldpath");
+        resultEndpoint.expectedHeaderReceived(HTTP_METHOD, GET);
+
+        template.sendBody("seda:input", loadResourceAsStream("create.jsonld"));
+
+        assertMockEndpointsSatisfied();
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        context.getRouteDefinition("TrellisElasticsearchDeleter").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveAddLast().to("mock:results");
+                mockEndpointsAndSkip("http*");
+            }
+        });
+        context.start();
+
+        getMockEndpoint("mock:http4:localhost").expectedMessageCount(1);
+
+        resultEndpoint.expectedMessageCount(1);
+        resultEndpoint.expectedHeaderReceived(HTTP_URI,
+                "https://elasticsearch.example.com/ldp/_doc/https%3A%2F%2Fldp.example.com%2Fresource");
+        resultEndpoint.expectedHeaderReceived(HTTP_METHOD, DELETE);
+
+        template.sendBody("seda:input", loadResourceAsStream("delete.jsonld"));
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void testNotActivityMessage() throws Exception {
+        context.start();
+
+        resultEndpoint.expectedMessageCount(0);
+
+        template.sendBody("seda:input", "{\"foo\": \"bar\"}");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void testNotJson() throws Exception {
+        context.start();
+
+        resultEndpoint.expectedMessageCount(0);
+
+        template.sendBody("seda:input", "foo = bar");
+
+        assertMockEndpointsSatisfied();
+    }
+}
+
